1baa026de66251c2c4a2ddfc7b24974a5f0d99bc
18368262 <18368262@CAB-WSN-0014373>
2020-04-16 23:15:03 +0300

    refactor

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,10 @@
+// метод, создающий тэг по входящим параметрам
+// создан в целях обёртки над длинными методами js-a
+// name - имя тега
+// id - id
+// className - имя класса
+// innerText - innerText
+// innerHtml - innerHTML
 export const create = (name, id = null, className = null, innerText = null, innerHtml = null) => {
     const tag = document.createElement(name)
     if(id) {
@@ -15,20 +22,24 @@ export const create = (name, id = null, className = null, innerText = null, inne
     return tag
 }
 
+// проверка входящих значений
 export const check = (value) => {
+    // если значение пустое
     if(!value) {
         alert('Пустой таск')
         return false
     }
-
+    // получаем tbody
     const todoTableBody = document.getElementById('todoList');
+    // получаем всех детей внутри tbody и их первых детей (td с самим таском)
     let elements = todoTableBody.querySelectorAll('tr > td:first-child')
     for(let el of elements) {
+        // если хоть один из тасков равен входящему значению
         if(el.innerText === value) {
             alert('Такой таск уже имеется')
             return false
         }
     }
-    
+
     return true
 }
